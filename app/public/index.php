6b6467cfdbe1dcf2b307183c8cdbb1ec4e5c66e0
Simon Mitchell <kartano@gmail.com>
2017-08-16 17:23:10 -0500

    Modrewrite
    Added rewrite code for single point of truth.

--- app/public/index.php
+++ app/public/index.php
@@ -15,11 +15,26 @@ require_once dirname(__FILE__) . '/../bootstrap.php';
 //         Determine what page is requested.
 //          Handle accordingly.
 
+// Remember to adjust the DocumentRoot and modrewrite settings to get this to work.
+// Also remember the local .htaccess file.
+
+// SEE:  https://www.binpress.com/tutorial/php-bootstrapping-crash-course/146
+
 $page = $_SERVER['REQUEST_URI'];
 $halfValue = explode('.php/', $page);
-if (!empty($halfValue[1])) {
-    $getPageName = $halfValue[1];
-}
-$pageName = (empty($getPageName)) ? "Home" : $getPageName;
 
-echo $pageName;
+if (!empty($halfValue[1])) {
+    $pageName = $halfValue[1];
+} else {
+    $pageName="home";
+}
+
+// SM:  Use $pageHome at this point to switch to what part of the site is being requested.
+//      This is be either "home" if no URL was specified; or
+//
+
+echo "<pre>";
+print_r($page);
+print_r($halfValue);
+print_r($pageName);
+echo "</pre>";
